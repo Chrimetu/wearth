9603d4244674411c60ebcc50e446e3b36e1a38ef
Chirmera_tu <95748451+Chrimetu@users.noreply.github.com>
2022-09-05 17:31:04 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def get_ciba():
     return note_ch, note_en
  
  
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
+def send_message(user, accessToken, region, weather, temp, feelsLike, wind_dir, category, we_text, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -166,8 +166,20 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "value": temp,
                 "color": get_color()
             },
+            "feelsLike": {
+                "value": feelsLike,
+                "color": get_color()
+            },
             "wind_dir": {
                 "value": wind_dir,
+                "color": get_color()
+            },
+            "category": {
+                "value": category,
+                "color": get_color()
+            },
+            "we_text": {
+                "value": we_text,
                 "color": get_color()
             },
             "love_day": {
