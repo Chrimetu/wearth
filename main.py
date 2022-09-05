14fc2972376b882dd0995052f0a4256c96025e9b
Chirmera_tu <95748451+Chrimetu@users.noreply.github.com>
2022-09-05 15:54:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -214,6 +214,8 @@ if __name__ == "__main__":
         print("推送消息失败，请检查配置文件格式是否正确")
         os.system("pause")
         sys.exit(1)
+    
+
  
     # 获取accessToken
     accessToken = get_access_token()
